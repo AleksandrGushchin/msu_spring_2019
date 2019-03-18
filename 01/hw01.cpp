2fd776db7977e8e9c8762976cf710f7364b096bb
Aleksandr Gushchin <36573139+AleksandrGushchin@users.noreply.github.com>
2019-03-18 17:22:43 +0300

    Update hw01.cpp

--- 01/hw01.cpp
+++ 01/hw01.cpp
@@ -5,50 +5,43 @@
 
 using namespace std;
 
-void fill_Sieve(int* sieve)
+bool prime(int x)
 {
-    sieve[0] = 1;
-    int sq = sqrt(Size);
-    for(int i = 2; i < sq; ++i)
+    if(x == 2)
+        return true;
+    else if(x == 1 || !(x & 1))
+        return false;
+    int sq = sqrt(x) + 1;
+    for(int i = 3; i <= sq; ++i)
     {
-        if(!sieve[i - 1])
-        {
-            int i1 = 2 * i - 1;
-            while(i1 < Size)
-            {
-                sieve[i1] = 1;//0 = prime
-                i1 += i;      //1 = not prime
-            }
-        }
+        if(!(x % i))
+            return false;
     }
-    return;
+    return true;
 }
 
-int get_answ(int x, int y, int*& sieve)
+int get_answ(int x, int y)
 {
-    int left = 0, right = Size, count = 0, med;
-    while(Data[left] < x)
+    int i = 0, count = 0;
+    while(Data[i] < x)
+        i++;
+    if(Data[i] != x)
+        return -1;
+    while(Data[i] <= y)
     {
-        med = (left + right) / 2;
-        if(Data[med] <= x)
-            left = med;
-        else
-            right = med;
-    }
-    while(Data[left] >= x)
-        left--;
-    left++;
-    if(Data[left] != x)
-        return 0;
-
-    while(Data[left] <= y)
-    {
-        if(!sieve[Data[left] - 1])
+        bool pr = prime(Data[i]);
+        if(pr)
             count++;
-        left++;
+        while(Data[i] == Data[i + 1])
+        {
+            i++;
+            if(pr)
+                count++;
+        }
+        i++;
     }
-    if(Data[left - 1] != y)
-        return 0;
+    if(Data[i - 1] != y)
+        return -1;
     return count;
 }
 
@@ -57,8 +50,6 @@ int main(int argc, char* argv[])
     if(argc % 2 == 0 || argc == 1)
         return -1;
 
-    int *sieve = new int[Size]{0};
-    fill_Sieve(sieve);
     for (int i = 1; i < argc; i += 2)
     {
         int x = atoi(argv[i]);
@@ -68,9 +59,7 @@ int main(int argc, char* argv[])
             cout << 0;
             return 0;
         }
-        cout << get_answ(x, y, sieve) << endl;
+        cout << get_answ(x, y) << endl;
     }
-
-    delete []sieve;
     return 0;
 }
