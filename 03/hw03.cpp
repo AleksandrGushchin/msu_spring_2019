727a338544dcaba6d3d010533da973a9e9a14f11
Aleksandr Gushchin <36573139+AleksandrGushchin@users.noreply.github.com>
2019-03-31 22:21:16 +0300

    Create hw03.cpp

---
+++ 03/hw03.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <new>
+
+using namespace std;
+
+class Column
+{
+    int len;
+    int *arr;
+public:
+
+    Column(int c)
+        :len(c)
+    {
+        arr = new int[c];
+    }
+
+    int& operator [](int x)
+    {
+        if(x >= len)
+            throw out_of_range("");
+        return arr[x];
+    }
+
+    int& operator [](int x) const
+    {
+        if(x >= len)
+            throw out_of_range("");
+        return arr[x];
+    }
+};
+
+class Matrix
+{
+private:
+    Column *mat;
+    int rows, columns;
+
+public:
+    Matrix(int r, int c)
+        :rows(r),
+         columns(c)
+    {
+        mat = static_cast<Column*>(operator new[] (r * sizeof(Column)));
+        for (int i = 0; i < r; i++)
+        {
+              new (mat + i) Column(c);
+        }
+    }
+
+    int getRows() const
+    {
+        return rows;
+    }
+
+    int getColumns() const
+    {
+        return columns;
+    }
+
+    bool operator ==(const Matrix& other) const
+    {
+        if(columns != other.getColumns() || rows != other.getRows())
+            return false;
+        if(this == &other)
+            return true;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j  < columns; j++)
+                if((*this)[i][j] != other[i][j])
+                    return false;
+        }
+        return true;
+    }
+
+    bool operator !=(const Matrix& other) const
+    {
+        return !(*this == other);
+    }
+
+    Matrix& operator *=(int x){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++)
+                (*this)[i][j] = x * (*this)[i][j];
+        }
+        return *this;
+    }
+
+    Column& operator [](int x)
+    {
+        if(x >= rows)
+            throw out_of_range("");
+        return mat[x];
+    }
+
+    Column& operator [](int x) const
+    {
+        if(x >= rows)
+            throw out_of_range("");
+        //return const_cast<Column&>(mat[x]);
+        return mat[x];
+    }
+    ~Matrix()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            mat[i].~Column();
+        }
+        delete[] (mat);
+    }
+};
